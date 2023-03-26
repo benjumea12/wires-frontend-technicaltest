54279fe239987afb140701cb32c2c77fc577b643
Esteban Benjumea <benjumeac12@gmail.com>
2023-03-25 22:55:13 -0500

    add registry page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistryComponent } from './pages/registry/registry.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    title: 'Inicio',
+    component: HomeComponent,
+  },
+  {
+    path: 'registro',
+    title: 'Regístro',
+    component: RegistryComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
