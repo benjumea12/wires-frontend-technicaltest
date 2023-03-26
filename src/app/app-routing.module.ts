79dcf0fe1bee3892284cb340763ac546663263ef
Esteban Benjumea <benjumeac12@gmail.com>
2023-03-26 00:11:09 -0500

    change page home to start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistryComponent } from './pages/registry/registry.component';
+import { StartComponent } from './pages/start/start.component';
 
 const routes: Routes = [
   {
     path: '',
     title: 'Inicio',
-    component: HomeComponent,
+    component: StartComponent,
   },
   {
     path: 'registro',
