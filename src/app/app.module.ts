db21aa5ee676f359a71300575f247ec28e85f7dc
Esteban Benjumea <benjumeac12@gmail.com>
2023-03-25 22:55:03 -0500

    add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistryComponent } from './pages/registry/registry.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistryComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
